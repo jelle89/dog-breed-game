5a79eb07071cce5ea8c2f91cab21c922908be04b
Jelle Monen <jelle.monen@hotmail.com>
2019-06-19 11:37:10 +0200

    added homepage js file in components

---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="homepage">
+        <h1>Dog breed game</h1>
+        <p>click the link below to memorize the dog breeds and play the games</p>
+        <Link to="/breeds/list">Memorize dog breeds</Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
